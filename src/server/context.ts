8da36c550a635bbecafe4bde18e595efe8b32e27
phil395 <philiplgvn@gmail.com>
2022-12-28 21:35:50 +0300

    initialize prisma client, add tRPC context

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,1 +1,19 @@
-export { };
+import { inferAsyncReturnType } from "@trpc/server";
+import { CreateNextContextOptions } from "@trpc/server/adapters/next";
+import { prisma } from "../../db/prisma";
+// import { getSession } from 'next-auth/react';
+
+/**
+ * Creates context for an incoming request
+ * @link https://trpc.io/docs/context
+ */
+export const createContext = (opts: CreateNextContextOptions) => {
+	// function can be asynchronous
+	// const session = await getSession({ req: opts.req });
+
+	return {
+		prisma
+	};
+};
+
+export type Context = inferAsyncReturnType<typeof createContext>;
