75abf32e60b1f568ef588ae5b1ff5b0147450ae4
phil395 <philiplgvn@gmail.com>
2022-12-28 19:39:35 +0300

    clear template

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -2,7 +2,7 @@ import { httpBatchLink } from '@trpc/client';
 import { createTRPCNext } from '@trpc/next';
 import type { AppRouter } from '../server/routers/_app';
 
-function getBaseUrl() {
+const getBaseUrl = () => {
 	if (typeof window !== 'undefined')
 		// browser should use relative path
 		return '';
@@ -14,7 +14,7 @@ function getBaseUrl() {
 		return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
 	// assume localhost
 	return `http://localhost:${process.env.PORT ?? 3000}`;
-}
+};
 
 export const trpc = createTRPCNext<AppRouter>({
 	config({ ctx }) {
