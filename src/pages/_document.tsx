5e2e8b6b82d8a6c9eefc46cd3a7a6e342d04287f
phil395 <philiplgvn@gmail.com>
2022-12-29 16:08:58 +0300

    config tailwind, add font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document';
 const Document = () => {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@400;500;600&display=swap" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,31 @@ module.exports = {
     './src/components/**/*.tsx'
   ],
   theme: {
+    screens: {
+      'xs': '480px',
+      'sm': '576px',
+      'md': '768px',
+      'lg': '992px',
+      'xl': '1110px'
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'dark': '#121212',
+      'grey': {
+        100: '#F8F8F8',
+        500: '#E5E5E5',
+        900: '#9C9C9C'
+      },
+      'white': '#FFFFFF',
+      'yellow': '#F0CC84',
+      'pink': '#E91E63',
+      'red': '#D61313',
+      'green': '#0BB17F'
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
 }
