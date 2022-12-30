02f5ab51283f34f3271abf001baf146f6f2732f7
phil395 <philiplgvn@gmail.com>
2022-12-30 21:33:07 +0300

    add content: menu

---
+++ src/content/menu.ts
@@ -0,0 +1,106 @@
+export const MENU = [
+	{
+		title: 'About Us',
+		link: '#'
+	},
+	{
+		title: 'Women',
+		link: '/women',
+		catalog: [
+			{
+				group: 'tops',
+				links: [
+					{ name: 'Awesome', href: '/women' },
+					{ name: 'Beachwear', href: '/women' },
+					{ name: 'Beige', href: '/women' },
+					{ name: 'Cool', href: '/women' },
+					{ name: 'New Dress', href: '/women' },
+					{ name: 'Gap', href: '/women' },
+					{ name: 'Guess', href: '/women' },
+				]
+			},
+			{
+				group: 'bottoms',
+				links: [
+					{ name: 'Jeans', href: '/women' },
+					{ name: 'Lacoste', href: '/women' },
+					{ name: 'Levi\'s', href: '/women' },
+					{ name: 'Model', href: '/women' },
+					{ name: 'Nice Featured', href: '/women' },
+					{ name: 'Polo', href: '/women' },
+					{ name: 'Pullover', href: '/women' },
+				]
+			},
+			{
+				group: 'accessories',
+				links: [
+					{ name: 'Scarf Sale 13%', href: '/women' },
+					{ name: 'Shirt', href: '/women' },
+					{ name: 'Shoes', href: '/women' },
+					{ name: 'Shorts', href: '/women' },
+					{ name: 'Summer', href: '/women' },
+					{ name: 'Sunglasses', href: '/women' },
+					{ name: 'Vintage', href: '/women' },
+				]
+			},
+		]
+	},
+	{
+		title: 'Man',
+		link: '/man',
+		catalog: [
+			{
+				group: 'tops',
+				links: [
+					{ name: 'Awesome', href: '/man' },
+					{ name: 'Beachwear', href: '/man' },
+					{ name: 'Beige', href: '/man' },
+					{ name: 'Cool', href: '/man' },
+					{ name: 'New Dress', href: '/man' },
+					{ name: 'Gap', href: '/man' },
+					{ name: 'Guess', href: '/man' },
+				]
+			},
+			{
+				group: 'bottoms',
+				links: [
+					{ name: 'Jeans', href: '/man' },
+					{ name: 'Lacoste', href: '/man' },
+					{ name: 'Levi\'s', href: '/man' },
+					{ name: 'Model', href: '/man' },
+					{ name: 'Nice Featured', href: '/man' },
+					{ name: 'Polo', href: '/man' },
+					{ name: 'Pullover', href: '/man' },
+				]
+			},
+			{
+				group: 'accessories',
+				links: [
+					{ name: 'Scarf Sale 13%', href: '/man' },
+					{ name: 'Shirt', href: '/man' },
+					{ name: 'Shoes', href: '/man' },
+					{ name: 'Shorts', href: '/man' },
+					{ name: 'Summer', href: '/man' },
+					{ name: 'Sunglasses', href: '/man' },
+					{ name: 'Vintage', href: '/man' },
+				]
+			},
+		]
+	},
+	{
+		title: 'Beauty',
+		link: '#',
+	},
+	{
+		title: 'Accessories',
+		link: '#',
+	},
+	{
+		title: 'Blog',
+		link: '#',
+	},
+	{
+		title: 'Contacts',
+		link: '#',
+	}
+];
