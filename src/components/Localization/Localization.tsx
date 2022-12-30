8ddabea8d5396480357864d2580d41a5b8572f16
phil395 <philiplgvn@gmail.com>
2022-12-30 21:45:36 +0300

    add skeletons for future components

---
+++ src/components/Localization/Localization.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+interface Props {
+
+}
+
+export const Localization: FC<Props> = () => {
+	return (
+		<div className="">
+			Localization component
+		</div>
+	);
+};
---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+interface Props {
+
+}
+
+export const Search: FC<Props> = () => {
+	return (
+		<div className="text-white">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet eos qui velit illum facilis cupiditate deleniti rerum enim, molestias cumque. Molestiae similique id delectus ex temporibus magni error voluptas a.
+		</div>
+	);
+};
