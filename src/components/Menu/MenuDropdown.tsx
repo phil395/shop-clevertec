cea0b63668abe4eb04e66c283521842a4bf5a642
phil395 <philiplgvn@gmail.com>
2023-01-02 16:26:52 +0300

    refacted MenuDropdown component

--- src/components/Menu/MenuDropdown.tsx
+++ src/components/Menu/MenuDropdown.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { Menu } from "../../content";
+import { Bestsellers } from "./Bestsellers";
 
 type PointerEvents = Pick<
 	React.HTMLAttributes<HTMLDivElement>,
@@ -12,32 +13,35 @@ export interface MenuDropdownProps extends PointerEvents {
 
 export const MenuDropdown: FC<MenuDropdownProps> = ({ dropdown: { title, catalog }, ...pointerEvents }) => {
 	return (
-		<dialog open {...pointerEvents} className="py-7 w-full bg-grey-100">
-			<nav className="container flex">
-				{catalog.map((column, index) => (
-					<div
-						key={index}
-						className="flex-1"
-					>
-						<h3 className="uppercase font-semibold mb-3">{column.group}</h3>
+		<dialog open {...pointerEvents} className="w-full px-0 py-7 bg-grey-100 before:bg-dark before:h-[2px] before:w-full before:absolute before:top-0">
+			<div className="container flex">
+				<nav className="flex flex-1 mr-4">
+					{catalog.map((column, index) => (
+						<div
+							key={index}
+							className="flex-1"
+						>
+							<h3 className="uppercase font-semibold mb-3">{column.group}</h3>
 
-						<ul>
-							{column.links.map((link, index) => (
-								<li
-									key={index}
-									className="not-last:mb-2 text-dark/80"
-								>
-									<a href={link.href}>{link.name}</a>
-								</li>
-							))}
-						</ul>
-					</div>
-				))}
-			</nav>
+							<ul>
+								{column.links.map((link, index) => (
+									<li key={index}>
+										<a
+											href={link.href}
+											title={link.name}
+											className="py-2 block text-dark/80"
+										>
+											{link.name}
+										</a>
+									</li>
+								))}
+							</ul>
+						</div>
+					))}
+				</nav>
 
-
-			{/* <Bestsellers /> */}
-
+				<Bestsellers catalogTitle={title} />
+			</div>
 		</dialog>
 	);
 };
