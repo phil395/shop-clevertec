e3fe003b645a7f81e0ab6fa2765326804003baed
phil395 <philiplgvn@gmail.com>
2023-01-03 18:10:46 +0300

    add ProductPage draft

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,7 @@ const Document = () => {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@400;500;600&display=swap" rel="stylesheet" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
         <Main />
