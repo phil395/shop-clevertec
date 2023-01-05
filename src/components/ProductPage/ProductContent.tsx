f2cedad0da719d941a911fb8316bb99e2a7f75e3
phil395 <philiplgvn@gmail.com>
2023-01-05 18:48:37 +0300

    filling ProductContent component

--- src/components/ProductPage/ProductContent.tsx
+++ src/components/ProductPage/ProductContent.tsx
@@ -1,5 +1,7 @@
-import type { FC } from "react";
+import { Prisma } from "@prisma/client";
+import { FC, useState } from "react";
 import { trpc } from "../../utils/trpc";
+import { ProductSlider } from "./ProductSlider";
 
 interface Props {
 	sku: number;
@@ -20,14 +22,37 @@ export const ProductContent: FC<Props> = ({ sku }) => {
 		}
 	});
 
+	const [colorId, setColorId] = useState<number>();
+	const [sizeId, setSizeId] = useState<number>();
+
+	// console.dir(data);
+
 	if (!data) return null;
 
+	const images = data.products.flatMap(product => {
+		const isCorrectColor = colorId && product.colorId === colorId;
+		const isCorrectSize = sizeId && product.sizeId === sizeId;
+
+		const addImages = () => product.images.map(img => img);
+
+		if (!colorId && !sizeId) return addImages();
+		if (isCorrectColor && isCorrectSize) return addImages();
+		if (!colorId && isCorrectSize) return addImages();
+		if (isCorrectColor && !sizeId) return addImages();
+
+		return []; // not add
+	});
+
+
+
 	return (
-		<section>
-			Hello from ProductContent
-			<pre>
-				{JSON.stringify(data, null, 2)}
-			</pre>
+		<section className="md:py-7">
+			<div className="container">
+				<div className="flex space-x-6">
+					<ProductSlider images={images} className='flex-1 basis-1/2' />
+					<div className='flex-1 basis-1/2'>dsds</div>
+				</div>
+			</div>
 		</section>
 	);
 };
