b9a129989bee9345d8ad62af1d70d414882edb2f
phil395 <philiplgvn@gmail.com>
2023-01-07 20:58:28 +0300

    refacted ProductSlider component

--- src/components/ProductPage/ProductSlider.tsx
+++ src/components/ProductPage/ProductSlider.tsx
@@ -8,26 +8,26 @@ import { useSlider } from "../../hooks";
 
 interface Props {
 	className: string;
-	images: ImageType[];
+	imageUrls: string[] /* ImageType[] */;
 }
 
-export const ProductSlider: FC<Props> = ({ className, images }) => {
+export const ProductSlider: FC<Props> = ({ className, imageUrls }) => {
 
-	const { initializeElements, initializePreviews, nextSlide, prevSlide, selectElement, subscribeOnFirst, subscribeOnLast } = useSlider();
+	const { elementsRef, previewsRef, nextSlide, prevSlide, selectElement, subscribeOnFirst, subscribeOnLast } = useSlider();
 
 	return (
 		<figure className={className}>
 
-			<div className="flex sm:space-x-3 xl:space-x-5">
-				<div className="hidden sm:block">
-					<div className="flex justify-between">
+			<div className="flex max-sm:flex-col-reverse sm:space-x-3 xl:space-x-5">
+				<div className="max-sm:flex max-sm:mt-4">
+					<div className="flex justify-between max-sm:flex-col">
 						<SliderBtn
 							onClick={() => {
 								prevSlide();
 							}}
 							subscribe={subscribeOnFirst}
 							size={20}
-							className="p-2"
+							className="rotate-90 sm:rotate-180 p-2"
 						/>
 						<SliderBtn
 							onClick={() => {
@@ -35,19 +35,19 @@ export const ProductSlider: FC<Props> = ({ className, images }) => {
 							}}
 							subscribe={subscribeOnLast}
 							size={20}
-							className="rotate-180 p-2"
+							className="p-2 text-red max-sm:-rotate-90"
 						/>
 					</div>
 
-					<div ref={initializePreviews} className="h-[520px] flex flex-col space-y-7 scroll-smooth overflow-y-auto scrollbar-hide snap-y snap-mandatory">
-						{images.map((img, index) => (
+					<div ref={previewsRef} className="sm:h-[520px] flex flex-row sm:flex-col max-sm:space-x-4 sm:space-y-7 scroll-smooth overflow-auto scrollbar-hide max-sm:snap-x sm:snap-y snap-mandatory">
+						{imageUrls.map((img, index) => (
 							<button
-								key={img.id}
+								key={index}
 								className='h-28 w-[72px] snap-always snap-start flex-0'
 								onClick={() => selectElement(index)}
 							>
 								<Image
-									src={URL_IMG + img.url}
+									src={URL_IMG + img}
 									alt="product image"
 									className='object-cover w-full h-full'
 									width={96}
@@ -58,14 +58,14 @@ export const ProductSlider: FC<Props> = ({ className, images }) => {
 					</div>
 				</div>
 
-				<div ref={initializeElements} className="flex-grow flex space-x-6 scroll-smooth overflow-x-auto snap-x snap-mandatory scrollbar-hide bg-grey-100">
-					{images.map((img) => (
+				<div ref={elementsRef} className="flex-grow flex space-x-6 scroll-smooth overflow-x-auto snap-x snap-mandatory scrollbar-hide bg-grey-100">
+					{imageUrls.map((img, index) => (
 						<Image
-							key={img.id}
-							src={URL_IMG + img.url}
+							key={index}
+							src={URL_IMG + img}
 							alt="product image"
-							className="object-cover flex-0 snap-always snap-start"
-							width={380}
+							className="object-cover xl:w-full flex-0 snap-always snap-start"
+							width={440}
 							height={560}
 						/>
 					))}
