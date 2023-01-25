678b8ed40737d1941843c4c1f3b52ce3d1ff17bb
phil395 <philiplgvn@gmail.com>
2023-01-25 21:54:18 +0300

    add mobile dropdown in HomeShowcase

--- src/components/HomePage/HomeShowcase.tsx
+++ src/components/HomePage/HomeShowcase.tsx
@@ -1,6 +1,8 @@
+import { Listbox } from "@headlessui/react";
 import clsx from "clsx";
-import { FC, useMemo } from "react";
+import { FC, useMemo, useState } from "react";
 import { trpc } from "../../utils/trpc";
+import { Icon } from "../Icon";
 import { IProductCard } from "../ProductCard";
 import { Showcase } from "../Showcase";
 
@@ -8,8 +10,9 @@ interface Props {
 	className?: string;
 	catalog: 'men' | 'women';
 }
+type Feature = { name: string, key: string; };
 
-const CTA = [
+const FEATURES: Feature[] = [
 	{ name: 'New Arrivals', key: 'isNewArrivals' },
 	{ name: 'Specials', key: 'isSpecial' },
 	{ name: 'Bestsellers', key: 'isBestseller' },
@@ -18,6 +21,7 @@ const CTA = [
 ];
 
 export const HomeShowcase: FC<Props> = ({ className, catalog }) => {
+	const [selectedFeature, setSelectedFeature] = useState<Feature>(() => FEATURES[0]);
 	const { data: response, fetchNextPage } = trpc.products.list.useInfiniteQuery({ category: catalog, limit: 8 }, {
 		getNextPageParam: (lastPage, pages) => lastPage.nextCursor
 	});
@@ -42,17 +46,38 @@ export const HomeShowcase: FC<Props> = ({ className, catalog }) => {
 				<h2 className="flex-1 uppercase text-2xl text-dark font-semibold">
 					{catalog === 'men' ? 'MEN\'S' : 'WOMEN\'S'}
 				</h2>
-				{CTA.map(({ key, name }, index) => (
+
+
+				{FEATURES.map((feature, index) => (
 					<button
 						key={index}
-						onClick={() => key}
-						className={clsx("not-last:mr-6 uppercase text-dark/60 tracking-wider", {
-
+						onClick={() => setSelectedFeature(feature)}
+						className={clsx("not-last:mr-6 hidden uppercase text-dark/60 tracking-wider", {
+							['text-dark']: feature.key === selectedFeature.key
 						})}
 					>
-						{name}
+						{feature.name}
 					</button>
 				))}
+
+				<Listbox as={'div'} className="relative" value={selectedFeature} onChange={setSelectedFeature}>
+					<Listbox.Button>
+						{selectedFeature.name}
+						<Icon name="arrow" size={15} className="-mt-1 ml-3 inline-block" />
+					</Listbox.Button>
+					<Listbox.Options className="absolute right-0 bg-white shadow-md">
+						{FEATURES.map((feature, index) => (
+							<Listbox.Option
+								key={index}
+								value={feature}
+								className="py-2 px-5 ui-selected:text-dark text-dark/60 hover:bg-grey-100 cursor-pointer whitespace-nowrap"
+							>
+								{feature.name}
+							</Listbox.Option>
+						))}
+					</Listbox.Options>
+				</Listbox>
+
 			</div>
 			<Showcase cards={data} className="mt-4" />
 			<button
