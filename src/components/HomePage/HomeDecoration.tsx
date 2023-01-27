7e2828cb133ce38064f919290df6daa05f11efa2
phil395 <philiplgvn@gmail.com>
2023-01-27 19:31:42 +0300

    adaptation HomeDecoration

--- src/components/HomePage/HomeDecoration.tsx
+++ src/components/HomePage/HomeDecoration.tsx
@@ -16,14 +16,14 @@ const DECORATIONS = [
 	{
 		url: '/HomePage/home-decoration-2.jpg',
 		alt: 'home decoration 2',
-		title: [{ text: 'Get UP to', accent: false }, { text: '50% off', accent: true }],
+		title: [{ text: 'Get UP to', accent: false }, { text: '50 off', accent: true }],
 		subtitle: 'Sale'
 	},
 ];
 
 export const HomeDecoration: FC<Props> = () => {
 	return (
-		<div className="flex space-x-8 py-24">
+		<div className="flex max-lg:flex-col max-lg:space-y-4 lg:space-x-8 py-24">
 			{DECORATIONS.map(({ url, alt, title, subtitle }, index) => {
 				const titleParts = title.map(
 					({ text, accent }, index) => accent ? (
@@ -31,7 +31,7 @@ export const HomeDecoration: FC<Props> = () => {
 					) : text
 				);
 				return (
-					<figure key={index} className="relative">
+					<figure key={index} className="relative max-lg:h-60 lg:flex-1">
 						<Image src={url}
 							alt={alt}
 							width={540}
@@ -39,8 +39,8 @@ export const HomeDecoration: FC<Props> = () => {
 							className="object-cover w-full h-full"
 						/>
 						<figcaption className="pt-2 pb-1 px-7 absolute-center uppercase font-semibold bg-white">
-							<div className="text-sm text-dark/60">{subtitle}</div>
-							<div>{titleParts}</div>
+							<div className="text-sm text-center text-dark/60">{subtitle}</div>
+							<div className="whitespace-nowrap">{titleParts}</div>
 						</figcaption>
 					</figure>
 				);
